6c5d84c6368e2c9d17a259b622632d4230e48439
Andy Eka Mulya <andymulya13@gmail.com>
2024-01-14 19:23:00 +0700

    Menambahkan img logo pada fe

--- client/src/components/Logo.component.jsx
+++ client/src/components/Logo.component.jsx
@@ -1,10 +1,15 @@
+import logoMaestroBrain from '../assets/images/maestro_brain.png'
+
 const Logo  = () => {
     return (
         <>
-            <h1 className="font-bold text-lg text-blue-900 uppercase">
-                .Maestro
-                <span className="text-blue-700">Brain</span>
-            </h1>
+            <div className="flex items-center">
+                <img src={ logoMaestroBrain } alt="Logo Maestro Brain" width={40} />
+                <h1 className="font-bold text-md text-blue-900 uppercase">
+                    .Maestro
+                    <span className="text-blue-700">Brain</span>
+                </h1>
+            </div>
         </>
     )
 }
