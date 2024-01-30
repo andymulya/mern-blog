4c23509d65a8493fafb957963da30642725f8885
Andy Eka Mulya <andymulya13@gmail.com>
2024-01-30 20:56:33 +0700

    Memberikan logo di footer 404 page

--- client/src/pages/PageNotFound.page.jsx
+++ client/src/pages/PageNotFound.page.jsx
@@ -1,16 +1,22 @@
 import { Link } from "react-router-dom";
 import { Icon404 } from "../components/Icon.component";
+import Logo from "../components/Logo.component";
 
 export default function PageNotFound(){
     return (
-        <section className="h-cover flex justify-center p-4">
-            <div className="flex flex-col gap-5 items-center justify-center select-none aspect-square">
+        <section className="h-cover p-4 flex flex-col items-center justify-between select-none">
+            <section className="flex flex-col items-center gap-5">
                 <Icon404 />
                 <div className="text-center leading-10">
                     <h1 className="text-xl font-medium">Page not found</h1>
                     <p className="text-gray-500">The page you are lookig for does not exists. Head back to the <Link to={'/'} className="text-blue-700 font-medium hover:underline">home page</Link></p>
                 </div>
-            </div>
+            </section>
+
+            <section className="flex flex-col items-center">
+                <Logo />
+                <p className="text-sm text-gray-600">Read millions of stories around the world</p>
+            </section>
         </section>
     )
 }
